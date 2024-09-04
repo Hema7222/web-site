adce3fe9d2aa0a605eedcd04e558665529154e4b
Hema7222 <hemajeya004@gmail.com>
2024-09-04 14:36:09 +0530

    msg

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    eslintPlugin({
+      // Configure the plugin to not fail the build
+      failOnError: false,
+      failOnWarning: false
+    })
+  ],
   server: {
     port: 8001,
   },
